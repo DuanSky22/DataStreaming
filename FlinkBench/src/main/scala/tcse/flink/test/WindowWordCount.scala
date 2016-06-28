1e91c1621b231693d10db058e46167c1374ae165
DuanSky <duansky22@163.com>
2016-06-28 16:42:12 +0800

    modify flink bench pom file.

---
+++ FlinkBench/src/main/scala/tcse/flink/test/WindowWordCount.scala
@@ -0,0 +1,22 @@
+package tcse.flink.test
+
+/**
+  * Created by DuanSky on 2016/6/28.
+  */
+object WindowWordCount {
+  def main(args: Array[String]) {
+
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val text = env.socketTextStream("localhost", 9999)
+
+    val counts = text.flatMap { _.toLowerCase.split("\\W+") filter { _.nonEmpty } }
+      .map { (_, 1) }
+      .keyBy(0)
+      .timeWindow(Time.seconds(5))
+      .sum(1)
+
+    counts.print
+
+    env.execute("Window Stream WordCount")
+  }
+}
